dd8fc113f5ddb68d6421a7f2be47f83eb5283be4
Martin Jean-Christio <j.christio.martin@gmail.com>
2018-04-20 16:42:29 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -134,10 +134,12 @@ $( document ).ready( function(){
     var body = document.querySelector('body');
     if (win === 3) {
       $( '#alert' ).css( 'display', 'block' );
+      $('#game__choice').click(false);
       message.innerHTML = "You win 3 times ! Good job ! Click for new game !";
       body.style.backgroundColor = "rgba(22,22,22, 0.4)";
     } else if (lose === 3) {
       $( '#alert' ).css( 'display', 'block' );
+      $('#game__choice').click(false);
       message.innerHTML = "You lost 3 times, the end. Click for new game !";
       body.style.backgroundColor = "rgba(22,22,22, 0.4)";
     } else {
